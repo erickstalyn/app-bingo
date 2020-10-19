9173e43cc64906d7458e7f4bd8bc51ef318cd374
Stalyn <espacherrezpuyen@gmail.com>
2020-10-18 19:42:44 -0500

    login avance

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,6 +27,11 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
+    public function username()
+    {
+        return 'user';
+    }
+
     /**
      * Create a new controller instance.
      *
